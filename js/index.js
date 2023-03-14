b43adeb6189a12fb29d2b3c67fd1d1a6747d7e2c
waly2020 <walyguema@gmail.com>
2023-03-14 01:38:41 +0100

    nouvelle page d'acceuil

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-// if ($(".preloader").length > 0) {
-// Preloader timeout
-
 const preload = document.querySelector(".preloader");
 const navigation = document.querySelector(".navigation");
+const btns = document.querySelectorAll(".btn button");
+const cibles = document.querySelectorAll(".cible");
 
-setTimeout(function () {
-    // $('.preloader').addClass('d-none');
-    preload.classList.add("d-done");
-}, 1000);
-// };
+// setTimeout(function () {
+//     preload.classList.add("d-done");
+// }, 1000);
+
 window.addEventListener("scroll", () => {
     let { scrollY } = window;
     // console.dir(scrollY);
@@ -17,4 +15,18 @@ window.addEventListener("scroll", () => {
     } else {
         navigation.classList.remove("active");
     }
+})
+
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (!btn.classList.contains("active")) {
+            for (let i = 0; i < btns.length; i++) {
+                let b = btns[i];
+                cibles[i].classList.remove("active");
+                b.classList.remove("active");
+            }
+            btn.classList.add("active");
+            cibles[parseInt(btn.getAttribute("data-cible"))].classList.add("active");
+        }
+    })
 })
