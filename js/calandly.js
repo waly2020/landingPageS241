38ecb2632e2b858b479d5941f6f2ba17956f646b
maccbook AIR <maccbookair@maccbooks-MacBook-Air.local>
2023-06-13 16:33:29 +0100

    Pour visibilite s241

--- js/calandly.js
+++ js/calandly.js
@@ -1,7 +1,7 @@
 const content_calandly = document.querySelector(".content-calandly");
 const active_calandy = document.querySelectorAll(".active_calandly");
 Calendly.initInlineWidget({
-    url: 'https://calendly.com/doriancommunication/s241',
+    url: 'https://calendly.com/valentinovono/entretiens',
     parentElement: document.getElementById('calendly'),
     prefill: {},
     utm: {}
